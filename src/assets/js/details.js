51e0a194e42470fabbc75d55b66f51270b5fb274
DESKTOP-DFD9U2K\yepeizhan <751285829@qq.com>
2018-06-21 22:09:12 +0800

    add details

--- src/assets/js/details.js
+++ src/assets/js/details.js
@@ -13,27 +13,107 @@ export const CardsDetail = {
       ]
     },
     2: {
-      
+      image: '',
+      price: 500,
+      title: '',
+      intro:'',
+      release: '12个月',
+      include: [
+        {
+          project: '',
+          price: 450,
+        }
+      ]
     },
     3: {
-      
+        image: '',
+        price: 500,
+        title: '',
+        intro:'',
+        release: '12个月',
+        include: [
+          {
+            project: '',
+            price: 450,
+          }
+        ]
     },
     4: {
-      
+        image: '',
+        price: 500,
+        title: '',
+        intro:'',
+        release: '12个月',
+        include: [
+          {
+            project: '',
+            price: 450,
+          }
+        ]      
     },
     5: {
-      
+        image: '',
+        price: 500,
+        title: '',
+        intro:'',
+        release: '12个月',
+        include: [
+          {
+            project: '',
+            price: 450,
+          }
+        ]      
     },
     6: {
-      
+        image: '',
+        price: 500,
+        title: '',
+        intro:'',
+        release: '12个月',
+        include: [
+          {
+            project: '',
+            price: 450,
+          }
+        ]     
     },
     7: {
-      
+        image: '',
+        price: 500,
+        title: '',
+        intro:'',
+        release: '12个月',
+        include: [
+          {
+            project: '',
+            price: 450,
+          }
+        ]     
     },
     8: {
-      
+        image: '',
+        price: 500,
+        title: '',
+        intro:'',
+        release: '12个月',
+        include: [
+          {
+            project: '',
+            price: 450,
+          }
+        ]     
     },
     9: {
-      
+        image: '',
+        price: 500,
+        title: '',
+        intro:'',
+        release: '12个月',
+        include: [
+          {
+            project: '',
+            price: 450,
+          }
+        ]     
     },
 }
